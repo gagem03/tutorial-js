fafabe59c1e37a03b21c899dc0eebef9467d289d
gagem03 <gmacdonald21603@gmail.com>
2025-10-08 10:42:58 -0600

    updates to clock javascript file thing

--- tny_script.js
+++ tny_script.js
@@ -10,3 +10,12 @@
    Date:   October 2nd, 2025
 
 */
+
+// Display current date & time
+document.getElementById("dateNow").innerHTML = "m/d/yyyy<br />h:m:s";
+
+// Display the time left until New Years Eve Bash
+document.getElementById("days").textContent = "dd";
+document.getElementById("hrs").textContent = "hh";
+document.getElementById("mins").textContent = "mm";
+document.getElementById("secs").textContent = "ss";
